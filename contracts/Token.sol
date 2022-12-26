e9ead883f1be29f99024a7ab9da5e65d1f9c088e
Anna Revazova <62066807+jackie-max@users.noreply.github.com>
2022-12-26 14:04:52 +0300

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -23,32 +23,18 @@ contract CryptoBlockLab2 is ERC20 {
 
     constructor(uint256 initialSupply) ERC20("CryptoBlockLab2", "TSL") {
         _mint(msg.sender, initialSupply);
-//        struct DataStructure* p = (struct DataStructure*)0x8000000; // отображение адресов на структуру
-//        p->Logical = 0x1;
-//        p->SignedInteger = 0x2;
-//        p->Address = 0x3;
-//        p->FourBytes = 0x4;
-//        function fill_map(DataStructure Logical,DataStructure SignedInteger,DataStructure Address,DataStructure FourBytes)   {
 
     }
+    DataStructure[] public DStruct;
 
-//    mapping (address=>DataStructure[]) Map; // хранение отображения адресов, связанных с массивами структур
-//    function fill_map(DataStructure Logical,DataStructure SignedInteger,DataStructure Address,DataStructure FourBytes)   {
-//        Map[msg.sender].push(Logical);
-//        Map[msg.sender].push(SignedInteger);
-//        Map[msg.sender].push(Address);
-//        Map[msg.sender].push(FourBytes);
-//      }
+    mapping (address=>DataStructure[]) Map; // хранение отображения адресов, связанных с массивами структур
+    function fill_map(DataStructure Logical,DataStructure SignedInteger,DataStructure Address,DataStructure FourBytes) internal   {
+        Map[msg.sender].push(Logical)
+	Map[msg.sender].push(SignedInteger)
+	Map[msg.sender].push(Address)
+	Map[msg.sender].push(FourBytes)
+      }
 	
-	DataStructure[] public DStruct;
-    mapping(bool => bool) idBool; // сопоставление ID с индексом структуры
-    mapping(int => int) idInt; 
-    mapping(address => address) idAddr; 
-    mapping(uint32 => uint32) idBytes; 
-//    function getDStruct(uint256 id) public view returns (DataStructure memory) {
-//        uint256 index = idIndexMap[id];
-//        return DStruct[index];
-//    }
 
 
     modifier ModifyDataStructure(bool Logical) { // метод модификации данных
